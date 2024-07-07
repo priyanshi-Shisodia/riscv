5543db14f0fd7ddbf838cab2e7899388725c3f47
priyanshi-Shisodia <2022eeb1206@iitrpr.ac.in>
2024-07-07 20:58:56 +0530

    Add files via upload

---
+++ alu.sv
@@ -0,0 +1,12 @@
+
+module alu(
+    input logic [31:0] pc,
+    input logic [31:0] imm,
+    input logic [31:0] rs1_val,
+    input logic [31:0] rs2_val,
+    input logic [4:0] alu_control,
+    output logic rd_write_control,
+    output logic [31:0] rd_write_val
+);
+
+endmodule
